b424d92e2be15a92ffe03b666dfaef9e630ee74e
Blaz Rupnik <blaz.rupnik@celtra.com>
2020-12-30 10:07:30 +0100

    otsu for threshold, better visualization

--- cannyEdgeDetector.m
+++ cannyEdgeDetector.m
@@ -1,20 +1,18 @@
 function cannyEdgeDetector(image_name)
     % read image
     img = imread(image_name);
+    figure;
+    subplot(2, 2, 1); imshow(img); title('Original image')
     % 1. NOISE REDUCTION
     img_smoothed = imgaussfilt(im2double(img), 2);
-    %figure; imshow(img_smoothed, []); title('Gaussian smoothing');
+    subplot(2, 2, 2); imshow(img_smoothed); title('Gaussian smoothing');
     % 2. GRADIENT CALCULATION
     kx = [-1 0 1; -1 0 1; -1 0 1];
     ky = [-1 -1 -1; 0 0 0; 1 1 1];
     gx = conv2(img_smoothed, kx, 'same');
     gy = conv2(img_smoothed, ky, 'same');
-    %figure; imshow(gx, []); title('Horizontal gradient');
-    %figure; imshow(gy, []); title('Vertical gradient');
     mag = sqrt(gx.^2 + gy.^2);
     ang = atan2(gy, gx);
-    %figure; imshow(mag, [min(mag(:)),max(mag(:))]); title('Magnitude');
-    %figure; imshow(ang, [min(ang(:)),max(ang(:))]); title('Angle');
     % normalize angles
     ang = ang * 180/pi;
     % adjustment for negative directions
@@ -25,11 +23,10 @@ function cannyEdgeDetector(image_name)
     ang = adjustAngle(ang, length1, length2);
     % 3. NON MAXIMA SUPPRESSION
     suppressed = nonMaximaSuppression(mag, ang, length1, length2);
-    % figure; imshow(suppressed);
-    %ang(200:205,200:205)
+    subplot(2, 2, 3); imshow(suppressed); title('After Non-Maxima suppression');
     % 4. HYSTERESIS THRESHOLDING AND 8-CONNECTIVITY FOR WEAK PIXELS
-    T_Low = 0.05;
-    T_High = 0.15;
-    detected_edges = hysteresisThresholding(suppressed, length1, length2, T_Low, T_High);
-    figure; imshow(detected_edges, [0, 255]);
+    thresh = multithresh(img_smoothed);
+    thresh_low = thresh / 2;
+    detected_edges = hysteresisThresholding(suppressed, length1, length2, thresh, thresh_low);
+    subplot(2, 2, 4); imshow(detected_edges); title('Detected edges');
 end
